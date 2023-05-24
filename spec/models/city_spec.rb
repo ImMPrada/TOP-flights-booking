b0e9c4569ee3b754683b1e8f52f57055f6671cd7
immprada <im.mprada@gmail.com>
2023-05-24 16:24:10 -0500

    upgrade city model

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe City, type: :model do
     it { should belong_to(:country) }
     it { should have_many(:airports) }
   end
+
+  describe '.all_cities_with_country_symbol' do
+    before { create(:city, country: create(:country, symbol: 'BR')) }
+
+    it 'should return all cities with country symbol' do
+      expect(City.all_cities_with_country_symbol.first.country_symbol).to eq('BR')
+    end
+  end
 end
