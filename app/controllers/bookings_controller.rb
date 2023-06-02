a5c07f32b8f237a3e5821019e2e2355aa8493587
immprada <im.mprada@gmail.com>
2023-06-02 12:11:56 -0500

    add bookings#new form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,28 @@
 class BookingsController < ApplicationController
-  def new; end
+  before_action :authenticate_user!
+
+  def new
+    @booking = Booking.new
+    if current_user_passenger
+      @booking.passengers << current_user_passenger
+    else
+      @booking.passengers << Passenger.new(user: current_user)
+    end
+    @booking.number = "#{flight.number}-#{DateTime.now.to_i}"
+    @booking.flight = flight
+  end
+
+  def create
+    byebug
+  end
+
+  private
+
+  def current_user_passenger
+    @current_user_passenger || current_user.passenger
+  end
+
+  def flight
+    @flight ||= Flight.find(params[:flight_id])
+  end
 end
